a546a960fb362bcdcc29a652b070c2da675d3d37
Kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2025-09-19 14:28:36 +0200

    cloudflare testing

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,6 +10,16 @@ declare namespace App {
 	interface Locals {
 		userid: string;
 	}
+
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache }
+	}
 	
 	// interface Platform {}
 	
